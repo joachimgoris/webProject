d86c261529105b5b3fbd579bde1b64bf8a298af3
Joachim <gorisjoachim@hotmail.com>
2018-05-22 11:33:14 +0200

    1

--- resources/ploegen/main.js
+++ resources/ploegen/main.js
@@ -1,4 +1,4 @@
 function ReadPloegen () {
-    var landen = JSON.parse("ploegen.json");
+    var landen = JSON.parse(ploegen.json);
        document.getElementById("landenLijst").innerHTML = landen.forEach(element => {name +" "+flag});
 }
